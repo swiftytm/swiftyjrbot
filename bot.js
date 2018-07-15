f49e316c5aee2509b003c234260c1171ee2c684c
Chase Warren <chasew4741@yahoo.com>
2018-07-15 06:50:22 -0400

    n

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ fs.readdir("./cmds/", (err, files) => {
     jsfiles.forEach((f, i) => {
         let props = require(`./cmds/${f}`);
         console.log(`${i + 1}: ${f} loaded!`);
-        bot.commands.set(props.help);
+        bot.commands.set(props.help.name, props);
 
     });
 
